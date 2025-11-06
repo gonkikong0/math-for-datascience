da7a05aab7594c5a0e3682a3faaab30627449141
gonkikong0 <toshivy@outlook.com>
2025-11-06 21:29:51 +0530

    Functions

---
+++ Basic Math and Calculus/variable.py
@@ -0,0 +1,6 @@
+# Variable = Place holder for the unknown
+
+x = input("Enter a number ")
+product = int(x) * 5
+
+print(product)
---
+++ functions.py
@@ -0,0 +1,10 @@
+#Functions - Help us model a linear relationship between variables. (y = 2x + 1 OR f(x) = 2x + 1)
+
+def f(x):
+    return 5*x + 1
+
+values_of_x = [0,1,2,3,4,5]
+
+for x in values_of_x:
+    y = f(x)
+    print(y)
